9415497600f621a8e4236bdb879d500ff0bdfec5
Alva Rani James <aalva@eth.ch>
2018-11-08 15:42:27 +0100

    Add existing file

--- part1_test.sh
+++ part1_test.sh
@@ -3,24 +3,27 @@
 arg_pass=0
 
 if [  $# -eq 0  ]; then
-	echo "Usage: sh `basename $0` <samplelistFile> <fastqDir> <outdir>"
+	echo "Usage: sh `basename $0` <samplelistFile> <fastqDir> <yml> <count> <alignment>"
 	echo "Example: "
-	echo "sh /cluster/home/aalva/Projects/PHRT-Immuno/scripts/useage_shell/part1_test.sh /cluster/home/aalva/Projects/PHRT-Immuno/scripts/useage_shell/sample_fastqs1 /cluster/work/grlab/projects/coukos_immuno/data/20180802/fastqs /cluster/home/aalva/Projects/PHRT-Immuno/results"
+	echo "sh /cluster/home/aalva/Projects/PHRT-Immuno/scripts/yml_maker/part1_test.sh /cluster/home/aalva/Projects/PHRT-Immuno/scripts/useage_shell/sample_fastqs1 /cluster/work/grlab/projects/coukos_immuno/data/20180802/fastqs /cluster/home/aalva/Projects/PHRT-Immuno/results/PART1/yml/ /cluster/home/aalva/Projects/PHRT-Immuno/results/counts/  /cluster/home/aalva/Projects/PHRT-Immuno/results/alignment/"
     arg_pass=1
 fi
 
 if [ ${arg_pass} -eq 0 ] ; then
     samplelistFile=$1
 	fastqDir=$2
-	outdir=$3
+	yml=$3
+    count=$4
+    alignment=$5
+    
 
 	#Making YAML files
-	python /cluster/home/aalva/Projects/PHRT-Immuno/scripts/yml_scripts/makeyml1.py ${samplelistFile} ${fastqDir} ${outdir}
+	python /cluster/home/aalva/Projects/PHRT-Immuno/scripts/yml_scripts/makeyml1.py ${samplelistFile} ${fastqDir} ${yml} ${count} ${alignment}
 
         source activating.sh
 	# Running the pipeline in cluster
 fi
-ls -1 *yml | xargs -I% echo bsub -M 60000 -W 12:00 -R "rusage[mem=60000]"  -J % -o %.job cwltool /cluster/home/aalva/Projects/PHRT-Immuno/scripts/cwl/pipline1_test.cwl %  >cmd.sh
+ls -1  ${yml}/*yml | xargs -I% echo bsub -M 60000 -W 12:00 -R "rusage[mem=60000]"  -J % -o %.job cwltool /cluster/home/aalva/Projects/PHRT-Immuno/scripts/cwl/pipline1_test.cwl %  >cmd.sh
 find -name 'activating.sh | xargs -I% echo source %'
 find -name 'cmd.sh' | xargs -I% sh %
 	
